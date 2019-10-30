e2b50a3acf64e067aed54409cf8e6c12db0c0952
CiprianCorneanu <cipriancorneanu@gmail.com>
2019-10-30 16:00:51 +0100

    MOdify data reading. Plot all and per noet and mark datasets.

--- main.py
+++ main.py
@@ -9,30 +9,25 @@ from sklearn import linear_model
 import seaborn as sns
 import pandas as pd
 
-root = "/Users/cipriancorneanu/Desktop/new_results/obj_rec/"
+root = "/Users/cipriancorneanu/Research/data/new_results/obj_rec/"
 n_nodes = {'mlp_300_100': 400, 'mlp_300_200_100': 600, 'conv_2': 650, 'conv_4': 906, 'alexnet': 1162, 'conv_6': 1418,
            'resnet18': 1736, 'vgg16': 1930, 'resnet34': 1736, 'resnet50': 6152}
 
 epcs = [290]
-TRIALS = [40, 50, 59, 68, 70, 71, 72]
-NETS = ['conv_2', 'conv_4', 'conv_6', 'alexnet', 'resnet18', 'vgg16']
-DATASETS = ['imagenet']
+TRIALS = [40, 45, 46, 47, 48, 49, 50, 56, 57, 58, 59, 64, 65, 66, 67, 68, 70, 71, 72]
+NETS = ['conv_2', 'conv_4', 'alexnet', 'conv_6', 'vgg16', 'resnet18']
+DATASETS = ['mnist', 'svhn', 'cifar10', 'cifar100', 'imagenet']
 
 ''' Plot AU rec'''
 def get_data(nets, trials):
-    pts = {}
+    pts = []
     for i_net, net in enumerate(nets):
-        pts[net] = {'ggap': [], 'betti': [], 'epsilon': [], 'trial': [], 'dataset':[]}
         for i_dataset, dataset in enumerate(DATASETS):
-            pts[net]['dataset'].append(dataset)
             directory = root + net + '_' + dataset + '/'
-
             if os.path.exists(directory):
                 for i, epc in enumerate([290]):
                     # If file exists, read and plot
                     for trial in trials:
-                        pts[net]['trial'].append(trial)
-
                         if os.path.exists(directory + 'adj_epc' + str(epc) + '_trl{}_0.4.bin.out'.format(trial)):
                             # read data
                             data = np.asarray([read_results(directory, epcs, trl=trl, dim=1, persistence=0.02
@@ -45,9 +40,12 @@ def get_data(nets, trials):
                                     loss = pkl.load(f)
                                     print(net, dataset, trial, loss[epc]['acc_tr'] - loss[epc]['acc_te'])
                                     if loss[epc]['acc_tr'] - loss[epc]['acc_te'] > 1 and loss[epc]['acc_tr'] - loss[epc]['acc_te'] < 100:
-                                        pts[net]['ggap'].append((loss[epc]['acc_tr'] - loss[epc]['acc_te']))
-                                        pts[net]['betti'].append(betti_max(x_ref, data[i]) / n_nodes[net])
-                                        pts[net]['epsilon'].append(epsilon_max(x_ref, data[i]))
+
+                                        ggap = (loss[epc]['acc_tr'] - loss[epc]['acc_te'])
+                                        betti = betti_max(x_ref, data[i]) / n_nodes[net]
+                                        epsilon = epsilon_max(x_ref, data[i])
+
+                                        pts.append({'net':net, 'dataset':dataset, 'ggap':ggap, 'trial':trial, 'betti':betti, 'epsilon':epsilon})
 
 
 
@@ -60,6 +58,13 @@ def get_data(nets, trials):
 
 
 data = get_data(NETS, TRIALS)
+
+
+''' Filter per nets '''
+data = [[item for item in data if item['net']==net] for net in NETS]
+
+
+
 fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(21, 9), sharex=True, sharey=False)
 axes.set_xlabel('$\Delta_{acc}[\%]$')
 axes.set_ylabel('$betti_1^{max}[cavs/node]$')
@@ -67,22 +72,52 @@ axes.grid(which='major', linestyle='--')
 axes.set_ylim([0,0.5])
 axes.set_xlim([0,1])
 
-for model in data:
-    x = np.asarray(data[model]['ggap'])/100
-    y = data[model]['epsilon']
-    #x = [np.power(item, 0.25) for item in x]
 
-    axes.scatter(x, y, label=model)
+for net in data:
+    x = [np.asarray(item['ggap'])/100 for item in net]
+    y = [item['epsilon'] for item in net]
+
+    axes.scatter(x, y, label=net[0]['net'])
 
     ''' Regression '''
-    '''
     sns.set(color_codes=True)
     tips = pd.DataFrame(list(zip(x, y)),
                columns =['ggap', 'epsilon'])
 
-    ax = sns.regplot(x="ggap", y="epsilon", data=tips, order=1)
-    '''
-
+    sns.regplot(x="ggap", y="epsilon", data=tips, order=1)
 
 fig.legend()
-fig.savefig(root + 'cvpr2020/all_p_0.02_lin_imagenet.png')
+fig.savefig(root + 'cvpr2020/all.png')
+
+
+
+for net in data:
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(21, 9), sharex=True, sharey=False)
+    axes.set_xlabel('$\Delta_{acc}[\%]$')
+    axes.set_ylabel('$betti_1^{max}[cavs/node]$')
+    axes.grid(which='major', linestyle='--')
+    axes.set_ylim([0,0.5])
+    axes.set_xlim([0,1])
+
+    x = [np.asarray(item['ggap'])/100 for item in net]
+    y = [item['epsilon'] for item in net]
+
+    ''' Regression '''
+    sns.set(color_codes=True)
+    tips = pd.DataFrame(list(zip(x, y)),
+               columns =['ggap', 'epsilon'])
+
+    sns.regplot(x="ggap", y="epsilon", data=tips, order=1)
+
+
+    for dataset in DATASETS:
+        a = [item for item in net if item['dataset']==dataset]
+
+        x = [np.asarray(item['ggap'])/100 for item in a]
+        y = [item['epsilon'] for item in a]
+
+        axes.scatter(x, y, label=dataset)
+
+
+    fig.legend()
+    fig.savefig(root + 'cvpr2020/'+net[0]['net']+'.png')
